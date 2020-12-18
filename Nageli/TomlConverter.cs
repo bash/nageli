7ec1769380697c663f56bde7a81976584b179a66
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-12-18 12:37:26 +0100

    Disallow implementing TomlConverter directly

--- Nageli/TomlConverter.cs
+++ Nageli/TomlConverter.cs
@@ -3,8 +3,13 @@ using Tomlyn.Model;
 
 namespace Nageli
 {
+    /// <remarks>Do not implement this type directly. Implement <see cref="TomlConverter{T}"/> instead.</remarks>
     public abstract class TomlConverter
     {
+        internal TomlConverter()
+        {
+        }
+
         public abstract object ConvertFrom(TomlObject value, Type typeToConvert, TomlSerializerOptions options);
 
         public abstract object ConvertFromAbsent(Type typeToConvert, TomlSerializerOptions options);
