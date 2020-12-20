211c7d704773b78e5498938bf343bc00bac6cf9e
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2020-12-20 22:54:30 +0100

    Simplify serializer context

--- Nageli/CachingSerializerContext.cs
+++ Nageli/CachingSerializerContext.cs
@@ -1,44 +1,36 @@
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Nageli
 {
     internal sealed class CachingSerializerContext : ITomlSerializerContext
     {
-        private readonly IDictionary<Type, ITomlConverter> _cachedConverters = new ConcurrentDictionary<Type, ITomlConverter>();
+        private readonly ConcurrentDictionary<Type, ITomlConverter> _cachedConverters = new();
 
-        private readonly IDictionary<Type, Type?> _cachedDefaultImplementations = new ConcurrentDictionary<Type, Type?>();
+        private readonly ConcurrentDictionary<Type, Type?> _cachedDefaultImplementations = new();
 
         public CachingSerializerContext(TomlSerializerOptions options) => Options = options;
 
         public TomlSerializerOptions Options { get; }
 
         public ITomlConverter GetConverter(Type typeToConvert)
-        {
-            if (_cachedConverters.TryGetValue(typeToConvert, out var cachedConverter))
-            {
-                return cachedConverter;
-            }
-
-            var factory = Options.Converters.First(c => c.CanConvert(typeToConvert));
-            var converter = factory.CreateConverter(typeToConvert, this);
-            _cachedConverters.TryAdd(typeToConvert, converter);
-            return converter;
-        }
+            => _cachedConverters.GetOrAdd(typeToConvert, GetConverterUncached);
 
         public Type? GetDefaultImplementation(Type typeToConvert)
-        {
-            if (_cachedDefaultImplementations.TryGetValue(typeToConvert, out var cachedImplementation))
-            {
-                return cachedImplementation;
-            }
+            => _cachedDefaultImplementations.GetOrAdd(typeToConvert, GetImplementationUncached);
 
+        private ITomlConverter GetConverterUncached(Type typeToConvert)
+        {
+            var factory = Options.Converters.FirstOrDefault(c => c.CanConvert(typeToConvert))
+                ?? throw new TomlException($"No matching converter found for type '{typeToConvert}'");
+            return factory.CreateConverter(typeToConvert, this);
+        }
+
+        private Type? GetImplementationUncached(Type typeToConvert)
+        {
             var provider = Options.DefaultImplementations.FirstOrDefault(c => c.HasDefaultImplementation(typeToConvert));
-            var implementation = provider?.GetDefaultImplementation(typeToConvert);
-            _cachedDefaultImplementations.TryAdd(typeToConvert, implementation);
-            return implementation;
+            return provider?.GetDefaultImplementation(typeToConvert);
         }
     }
 }
